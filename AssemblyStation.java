606cdf27341818213e20e000f0a4f4fb3909099d
Matias Vapola <matias.vapola@aalto.fi>
2021-01-26 15:45:20 +0200

    virheitä

--- AssemblyStation.java
+++ AssemblyStation.java
@@ -22,42 +22,47 @@ public class AssemblyStation {
     Node node = new Node();
     Geometry geom;
     Box box;
-    
+
     float maxHeight = 4; // max korkeus reitin välietapeille
     boolean moving = false; // true jos matkalla seuraavaan välietappiin
     Trejectory trajectory;
     RobotArm assemblyArm;
+    float legoSpacingX = 2; // legojen slottipaikkojen etäisyys 
+    float legoSpacingZ = 2;
+    float surfaceHeight;
 
     public AssemblyStation(AssetManager assetManager, Node rootNode, float xOffset, float zOffset, RobotArm Arm) {
         assemblyArm = Arm;
-        float yExtent = 6;   
+        float yExtent = 6;
         box = new Box(20, yExtent, 10);
         geom = new Geometry("Box", box);
         node.attachChild(geom);
-       
-        Material mat = new Material(assetManager,"Common/MatDefs/Light/Lighting.j3md");
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
         mat.setBoolean("UseMaterialColors", true);
         mat.setColor("Diffuse", ColorRGBA.LightGray);
         geom.setMaterial(mat);
-        geom.setLocalTranslation(xOffset, Main.floorHeight + yExtent , zOffset);
+        geom.setLocalTranslation(xOffset, Main.floorHeight + yExtent, zOffset);
+        surfaceHeight = Main.floorHeight + 2 * yExtent;
     }
+
     // tehdään APP eli reitinsuunnittelu destination koordinaatteihin
     public void initTestMove(Vector3f destination) {
         trajectory = new Trejectory();
-        
+
         // eka välietappi suoraan ylös max korkeuteen
         Vector3f v1 = assemblyArm.getToolTipLocation();
         v1.setY(maxHeight);
         trajectory.addPoint(v1);
         // toka välietappi max korkeuteen destination ylle
-        
+
         Vector3f v2 = new Vector3f();
         v2.setY(maxHeight);
         v2.setX(destination.getX());
         v2.setZ(destination.getZ());
-        
+
         trajectory.addPoint(v2);
-        
+        destination.setY(destination.getY() + 0.4f);
         trajectory.addPoint(destination);
         trajectory.initTrajectory();
     }
@@ -73,7 +78,7 @@ public class AssemblyStation {
         } else {
             // tänne tullaan jos edellinen välietappi saavutettiin
             //System.out.println("tähän asti päästään");
-           
+
             Vector3f nextPoint = trajectory.nextPoint();
             //System.out.println("tänne ei päästä");
             if (nextPoint == null) {
@@ -83,11 +88,34 @@ public class AssemblyStation {
                 //System.out.println(nextPoint);
                 // annetaan robotille seuraava välietappi ja alustetaan moving seuraavaa
                 // move() kutsua silmälläpitäen
-                
+
                 assemblyArm.initMove(nextPoint);
-                moving=true;
+                moving = true;
                 return true;
             }
         }
+        // kokoonpanoasemalla on slotteja, joiden indeksi on kokonaisluku
+        // tämä palauttaa slotin 3D koordinaatit
+    public Vector3f slotPosition(int slot) {
+        // vain osa asemasta on varattu tähän tarkoitukseen. Sen koko on 16x12
+        int rowSize = (int) ((16) / legoSpacingX);
+
+        int columnSize = (int) ((12) / legoSpacingZ);
+
+        int rowIndex = slot % rowSize;
+
+        float xOffset = (rowIndex - 1) * legoSpacingX;
+
+        int columnIndex = slot / rowSize;
+
+        float zOffset = (columnIndex + 2) * legoSpacingZ;
+
+        float yOffset = 0.4f;
+
+        // legonyExtent
+        // ’x’ja ’z’onfloat muuttujia, joihin on tallennettu konstruktorin xOffset/zOffset
+        // laske ’surfaceHeight’ konstruktorissa
+        return new Vector3f(x + xOffset, surfaceHeight + yOffset,z  + zOffset - 12);
     }
 }
+
