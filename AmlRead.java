9fa95056793dfd5000aa839ddbe017b1ba87cc4b
coodeex <juhani.s.lehto@gmail.com>
2021-02-09 14:49:20 +0200

    aml readii

---
+++ AmlRead.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author juhan
+ */
+public class AmlRead {
+
+    public void AmlRead(int legoBufferX) {
+        legoBufferX = 666;
+    }
+}
--- Main.java
+++ Main.java
@@ -39,37 +39,56 @@ public class Main extends SimpleApplication {
     // listan perusteella tiedetään missä järjestyksessä lajitellaan värin mukaan
     ArrayList<String> colors = new ArrayList(numColors);
     LegoBuffer pufferi;
+    int legoBufferX;
+    int legoBufferZ;
+    int assemblyStationX;
+    int assemblyStationZ;
+    
+    /////
+    /////
+    
+    boolean stackMode2 = false;
+
+    public static float floorHeight2 = -15;
+    public AssemblyStation Station2;
+
+    boolean freeze2 = false; // debug tarkoituksiin – laita true siinä kohdassa koodia
+
+    // mihin haluat robotin pysähtyvän
+    boolean moving2 = false; // true kun robotti liikkuu
+    boolean goingToLego2 = false; // true kun mennään hakemaan legoa bufferista
+    Lego lego2;
+    int slotIndex2 = 0; // kokoonpanoaseman slot
+    final int numColors2 = 4; // final on sama kuin C-kielen const
+    int colorIndex2 = 0; // “colors” (kts alla) listan indeksi
+    // listan perusteella tiedetään missä järjestyksessä lajitellaan värin mukaan
+    ArrayList<String> colors2 = new ArrayList(numColors2);
+    LegoBuffer pufferi2;
+    int legoBuffer2X;
+    int legoBuffer2Z;
+    int assemblyStation2X;
+    int assemblyStation2Z;
+    AmlRead AmlRead = new AmlRead();
+    
 
     @Override
     public void simpleInitApp() {
 
-        flyCam.setMoveSpeed(10);
+        flyCam.setMoveSpeed(20);
 
         RobotArm Arm = new RobotArm(assetManager, rootNode);
 
         rootNode.attachChild(Arm.node);
         Arm.node.attachChild(Arm.tooltipNode);
+        
+        AmlRead.AmlRead(legoBufferX);
+        System.out.println(legoBufferX + ": legoBufdferrrrxxxxxxxxxxx");
 
         Station = new AssemblyStation(assetManager, rootNode, 5, -11, Arm);
         rootNode.attachChild(Station.node);
 
         pufferi = new LegoBuffer(assetManager, rootNode, 5, -29, 10, 6);
 
-        /*Lego lego1 = new Lego(assetManager, "yellow");
-        rootNode.attachChild(lego1.node);
-        lego1.node.setLocalTranslation(2f, 0, 0);
-
-        Lego lego2 = new Lego(assetManager, "pink");
-        rootNode.attachChild(lego2.node);
-        lego2.node.setLocalTranslation(4f, 0, 0);
-
-        Lego lego3 = new Lego(assetManager, "");
-        rootNode.attachChild(lego3.node);
-        lego3.node.setLocalTranslation(6f, 0, 0);
-
-        Lego lego4 = new Lego(assetManager, "blue");
-        rootNode.attachChild(lego4.node);
-        lego4.node.setLocalTranslation(8f, 0, 0);*/
         PointLight lamp_light = new PointLight();
         lamp_light.setColor(ColorRGBA.White);
         lamp_light.setRadius(400f);
@@ -82,8 +101,38 @@ public class Main extends SimpleApplication {
         colors.add("blue");
         colors.add("pink");
         colors.add("green");
+        
+        
+        ///////////////////////////
+        ///////////////////////////
+        
+        
+        RobotArm Arm2 = new RobotArm(assetManager, rootNode);
+
+        rootNode.attachChild(Arm2.node);
+        Arm2.node.attachChild(Arm2.tooltipNode);
+
+        Station2 = new AssemblyStation(assetManager, rootNode, 15, -11, Arm2);
+        rootNode.attachChild(Station2.node);
+
+        pufferi2 = new LegoBuffer(assetManager, rootNode, 15, -29, 10, 6);
+
+        /*PointLight lamp_light2 = new PointLight();
+        lamp_light.setColor(ColorRGBA.White);
+        lamp_light.setRadius(400f);
+        lamp_light.setPosition(new Vector3f(2f, 8.0f, 10.0f));
+        rootNode.addLight(lamp_light2);*/
+
+        //Station.initTestMove(new Vector3f(7, -1, -31)); //HUOM: Poista initTestMove() kutsu.
+        colors2.add("yellow");
+        // lisää muut värit tehtävänannon mukaisessa järjestyksessä
+        colors2.add("blue");
+        colors2.add("pink");
+        colors2.add("green");
 
     }
+    
+
 
     @Override
     public void simpleUpdate(float tpf) {
@@ -149,7 +198,72 @@ public class Main extends SimpleApplication {
                 goingToLego = true;
             }
         }
+        
+        ///////////////
+        ///////////////
 
+        //Station.move(); tää rivi kommentoitu pois ku lisättiin part3 koodi:
+        if (!freeze2 && moving2) {
+            moving2 = Station2.move();
+        }
+        if (!moving2 && !freeze2) {
+            // moving=false tarkoittaa että saavuttiin reitin päähän, joten on 2 tapausta:
+            // otetaan lego mukaan tai jätetään se
+            if (goingToLego2) { // otetaan lego mukaan
+
+                // nyt ollaan bufferilla sen legon kohdalla mikä otetaan mukaan
+                // v:hen laitetaan kokoonpanoaseman slot numero ”slotIndex” koordinaatit
+                Vector3f v;
+                if (stackMode2) {
+                    v = Station2.stackSlotPosition(slotIndex2, lego2.color);
+                } else {
+                    v = Station2.slotPosition(slotIndex2);
+                }
+                slotIndex2++;
+
+                // suoritetaan APP kohteeseen v
+                Station2.initMoveToStation(lego2, v);
+                goingToLego2 = false;
+                moving2 = true;
+            } else { // jätetään lego tähän
+                if (lego2 != null) { // käynnistyksen yhteydessä tätä koodia ei suoriteta
+                    // lego on nyt toimitettu oikeaan paikkaan kokoonpanoasemalle
+                    // otetaan paikka talteen ennen kuin irrotetaan noodi
+                    Vector3f loc = lego2.node.getWorldTranslation();
+                    // irrota legon node tooltipin nodesta
+                    // (tämä on pitkä rimpsu jossa käytetään monen olion nimeä
+
+                    Station2.assemblyArm.tooltipNode.detachChild(lego2.node);
+
+                    lego2.node.setLocalTranslation(loc);
+                    // legon node ei ole nyt kiinni missään nodessa, joten se ei tule
+                    // näkyviin ennen kuin korjaat asian
+                    rootNode.attachChild(lego2.node);
+                }
+                // haetaan bufferista seuraava lego, jonka väri on: colors.get(colorIndex)
+                // eli päivitä muuttujan ’lego’ arvo                    rootNode.attachChild(lego.node);
+                lego2 = pufferi2.giveLego(colors2.get(colorIndex2));
+                //???
+                moving2 = true;
+                if (lego2 == null) {
+                    // bufferissa ei ole enempää tämänvärisiä legoja
+                    colorIndex2++;
+                    if (colorIndex2 >= numColors2) {
+                        // kaikki legot on siirretty
+                        freeze2 = true; // tämän jälkeen mitään ei tapahdu
+                    } else {
+                        // haetaan bufferista seuraava lego, jonka väri on:
+                        // colors.get(colorIndex)
+                        //?  ?  ?
+                        lego2 = pufferi2.giveLego(colors2.get(colorIndex2));
+                    }
+                }
+                if (!freeze2) {
+                    Station2.initMoveToLego(lego2);
+                }
+                goingToLego2 = true;
+            }
+        }
     }
 
     @Override
